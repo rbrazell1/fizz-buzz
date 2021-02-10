008bcd37131149c9f63cadb416f645378e35f37d
rbrazell1 <brazellrussell@gmail.com>
2021-02-10 11:35:03 -0700

    Added getUpperLimit and user input

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -1,24 +1,49 @@
 package edu.cnm.deepdive;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
 
   public static void main(String[] args) {
-  // TODO Get input from args or user
-  // TODO loop from 1 to upper limit (input) & invoke .getFizzBuzz  iteratively
+    int upperLimit;
+    if (args.length > 0) {
+      upperLimit = Integer.parseInt(args[0]);
+      emitResults(upperLimit);
+    } else {
+      do {
+        upperLimit = getUpperLimit();
+        emitResults(upperLimit);
+      } while (upperLimit > 0);
+    }
+  }
+
+  private static void emitResults(int upperLimit) {
+    for (int i = 1; i <= upperLimit; i++) {
+      System.out.println(getFizzBuzz(i));
+    }
   }
 
   public static String getFizzBuzz(int inputNum) {
     String result;
     if (inputNum % 15 == 0) {
       result = "FizzBuzz!";
-    } else if (inputNum % 5 ==0) {
+    } else if (inputNum % 5 == 0) {
       result = "Buzz!";
     } else if (inputNum % 3 == 0) {
       result = "Fizz!";
     } else {
       result = Integer.toString(inputNum);
     }
-    return  result;
+    return result;
+  }
 
+  private static int getUpperLimit() {
+    int result;
+    Scanner scanner = new Scanner(System.in);
+    do {
+      System.out.println("Please enter a positive upper limit number:");
+      result = scanner.nextInt();
+    } while (result < 0);
+    return result;
   }
 }
